a6579e59dc4aba9dcc6943ad6ed27d51fcfa014f
Naveen2706 <naveen271996@gmail.com>
2021-06-04 02:22:41 +0530

    loop for multiple matches

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,8 +1,20 @@
 This problem displays the winner Heads or Tails
 
+loop=0
+heads=0
+tails=0
+
+while [[ loop -le 10 ]];do
+
 x=$((RANDOM%2))
 if [[ $x -eq 0 ]];then
- echo winner : heads
+	echo winner : heads
+   ((heads++))
 else 
-echo winner : tails
+	echo winner : tails
+   ((tails++))
 fi
+((loop++))
+done
+echo heads :$heads
+echo tails :$tails
