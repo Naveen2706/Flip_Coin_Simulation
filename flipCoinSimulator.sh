cfe24be865c031a4e4dbccd9dfc92b7cec603f46
Naveen2706 <naveen271996@gmail.com>
2021-06-04 02:35:36 +0530

    21 matches winner simulation

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -4,7 +4,7 @@ loop=0
 heads=0
 tails=0
 
-while [[ loop -le 10 ]];do
+while [[ $heads -le 20 ]] && [[ $tails -le 20 ]];do
 
 x=$((RANDOM%2))
 if [[ $x -eq 0 ]];then
@@ -14,7 +14,14 @@ else
 	echo winner : tails
    ((tails++))
 fi
-((loop++))
 done
 echo heads :$heads
 echo tails :$tails
+
+if [[ $heads -gt $tails ]];then
+	echo heads won :"$heads"matches
+elif [[ $heads -eq $tails ]];then
+        echo its tie   :"$heads"heads & "$tails"tails
+else
+    	echo tails won :"$tails"matches
+fi
